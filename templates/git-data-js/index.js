71aa1deb6217b70bd6035ed9084a8a3c0466bf89
Bex Warner <bexmwarner@gmail.com>
2018-10-10 19:21:09 -0400

    finalize git data example and add test

--- templates/git-data-js/index.js
+++ templates/git-data-js/index.js
@@ -1,35 +1,55 @@
-// Git Data API example
+// Git Data API use case example
 // See: https://developer.github.com/v3/git/ to learn more
 module.exports = app => {
-  // Let's listen on every new team that gets created and add a file for them.
-  app.on('team.created', check)
+  // Opens a PR every time someone installs your app for the first time
+  app.on('installation.created', check)
   async function check (context) {
-    // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
-    // Get current reference in Git
-    const reference = await context.github.gitdata.getReference(context.repo({ ref: 'heads/master' }))
-    // Create a branch
-    const getBranch = await context.github.gitdata.createReference(context.repo({
-      ref: `refs/heads/new-team-${context.payload.team.name}`,
-      sha: reference.data.object.sha // accesses the sha from the heads/master reference we got
-    }))
-    // create a new file
-    const file = await context.github.repos.createFile(context.repo({
-      path: 'path/to/your/file.md', // the path to your config file
-      message: 'adds my team\'s file', // a commit message
-      content: `New team incoming! Check out ${context.payload.team.name}!`, //the content of your file
-      branch: `new-team-${context.payload.team.name}` // the branch name we used when creating a Git reference
-    }))
-    // create a PR from that branch with the new file
-    return await context.github.pullRequests.create(context.repo({
-      title: 'Adding my team\'s file!', // the title of the PR
-      head: `new-team-${context.payload.team.name}`, // the branch our chances are on
-      base: 'master', // the branch to which you want to merge your changes
-      body: 'Adds my teams\'s new file!', // the body of your PR,
-      maintainer_can_modify: true // allows maintainers to edit your app's PR
-    }))
-  }
-}
+    // shows all repos you've installed the app on
+    console.log(context.payload.repositories)
 
+    const owner = context.payload.installation.account.login
+    context.payload.repositories.forEach(async (repository) => {
+      const repo = repository.name
+
+      // Generates a random number to ensure the git reference isn't already taken
+      // NOTE: this is not recommended and just shows an example so it can work :)
+      const branch = `new-branch-${Math.floor(Math.random() * Math.floor(9999))}`
+
+      // Get current reference in Git
+      const reference = await context.github.gitdata.getReference({
+        repo, // the repo
+        owner, // the owner of the repo
+        ref: 'heads/master'
+      })
+      // Create a branch
+      await context.github.gitdata.createReference({
+        repo,
+        owner,
+        ref: `refs/heads/${branch}`,
+        sha: reference.data.object.sha // accesses the sha from the heads/master reference we got
+      })
+      // create a new file
+      await context.github.repos.createFile({
+        repo,
+        owner,
+        path: 'path/to/your/file.md', // the path to your config file
+        message: 'adds config file', // a commit message
+        content: Buffer.from('My new file is awesome!').toString('base64'),
+        // the content of your file, must be base64 encoded
+        branch // the branch name we used when creating a Git reference
+      })
+      // create a PR from that branch with the commit of our added file
+      await context.github.pullRequests.create({
+        repo,
+        owner,
+        title: 'Adding my file!', // the title of the PR
+        head: branch, // the branch our chances are on
+        base: 'master', // the branch to which you want to merge your changes
+        body: 'Adds my new file!', // the body of your PR,
+        maintainer_can_modify: true // allows maintainers to edit your app's PR
+      })
+    })
+  }
   // For more information on building apps:
   // https://probot.github.io/docs/
 
--- templates/git-data-js/test/index.test.js
+++ templates/git-data-js/test/index.test.js
@@ -1,42 +1,70 @@
-const { Application } = require('probot')
+const nock = require('nock')
 // Requiring our app implementation
 const myProbotApp = require('..')
+const { Probot } = require('probot')
+// Requiring our fixtures
+const installationCreatedPayload = require('./fixtures/installation.created')
 
-const issuesOpenedPayload = require('./fixtures/issues.opened.json')
+// Mocking out our use of random numbers
+const mockMath = Object.create(global.Math)
+mockMath.random = () => 1
+global.Math = mockMath
 
-test('that we can run tests', () => {
-  // your real tests go here
-  expect(1 + 2 + 3).toBe(6)
-})
+nock.disableNetConnect()
 
 describe('My Probot app', () => {
-  let app, github
+  let probot
 
   beforeEach(() => {
-    app = new Application()
-    // Initialize the app based on the code from index.js
-    app.load(myProbotApp)
-    // This is an easy way to mock out the GitHub API
-    github = {
-      issues: {
-        createComment: jest.fn().mockReturnValue(Promise.resolve({}))
-      }
-    }
-    // Passes the mocked out GitHub API into out app instance
-    app.auth = () => Promise.resolve(github)
+    probot = new Probot({})
+    // Load our app into probot
+    const app = probot.load(myProbotApp)
+
+    // just return a test token
+    app.app = () => 'test'
   })
 
-  test('creates a comment when an issue is opened', async () => {
-    // Simulates delivery of an issues.opened webhook
-    await app.receive({
-      event: 'issues.opened',
-      payload: issuesOpenedPayload
-    })
+  test('creates a pull request on installation', async () => {
+    nock('https://api.github.com')
+      .post('/app/installations/2/access_tokens')
+      .reply(200, { token: 'test' })
 
-    // This test passes if the code in your index.js file calls `context.github.issues.createComment`
-    expect(github.issues.createComment).toHaveBeenCalled()
+    nock('https://api.github.com')
+      .get('/repos/hiimbex/testing-things/git/refs/heads/master')
+      .reply(200, { object: { sha: 'abc123' } })
+
+    nock('https://api.github.com')
+      .post('/repos/hiimbex/testing-things/git/refs', {
+        ref: 'refs/heads/new-branch-9999',
+        sha: 'abc123'
+      })
+      .reply(200)
+
+    nock('https://api.github.com')
+      .put('/repos/hiimbex/testing-things/contents/path/to/your/file.md', {
+        branch: 'new-branch-9999',
+        message: 'adds config file',
+        content: 'TXkgbmV3IGZpbGUgaXMgYXdlc29tZSE='
+      })
+      .reply(200)
+
+    nock('https://api.github.com')
+      .post('/repos/hiimbex/testing-things/pulls', {
+        title: 'Adding my file!',
+        head: 'new-branch-9999',
+        base: 'master',
+        body: 'Adds my new file!',
+        maintainer_can_modify: true
+      })
+      .reply(200)
+
+    // Recieve a webhook event
+    await probot.receive({ name: 'installation', payload: installationCreatedPayload })
   })
 })
 
 // For more information about testing with Jest see:
 // https://facebook.github.io/jest/
+
+// For more information about testing with Nock see:
+// https://github.com/nock/nock
