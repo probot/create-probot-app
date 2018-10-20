e00e5be5230ba3c3db7dad5c383b36f36b54442d
Bex Warner <bexmwarner@gmail.com>
2018-10-20 07:02:39 -0400

    this is definitely how this works

--- templates/basic-ts/src/index.ts
+++ templates/basic-ts/src/index.ts
@@ -1,4 +1,4 @@
-import { Application } from 'probot'
+import { Application } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
   app.on('issues.opened', async (context) => {
