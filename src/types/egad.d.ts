0b2e27367f3956708d673f20242205fd88d06f77
Gregor Martynus <gregor@martynus.net>
2020-08-13 15:20:43 -0700

    build: update all dependencies

--- src/types/egad.d.ts
+++ src/types/egad.d.ts
@@ -1,13 +1,13 @@
-interface OptionsI {
+interface Options {
   /**
    * Set to `false` to avoid overwriting existing files.
    */
   overwrite?: boolean;
 }
 
-interface FileResultI {
+interface FileResult {
   path: string;
   skipped: boolean;
 }
 
-export function generate(source: string, dest: string, data?: {}, opts?: OptionsI): Promise<FileResultI[]>
+export function generate(source: string, dest: string, data?: Record<string, unknown>, opts?: Options): Promise<FileResult[]>
