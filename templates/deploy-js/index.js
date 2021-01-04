6174b90db93cc7bb661c27b1076763b49439ff9f
Alex <shaftoe@users.noreply.github.com>
2021-01-04 11:42:53 +0100

    fix: upgrade template apps to v11

--- templates/deploy-js/index.js
+++ templates/deploy-js/index.js
@@ -3,9 +3,9 @@
 
 /**
  * This is the main entrypoint to your Probot app
- * @param { {app: import('probot').Application} } app
+ * @param {import('probot').Probot} app
  */
-module.exports = ({ app }) => {
+module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
   app.on(
