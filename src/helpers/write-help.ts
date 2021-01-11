6036a31b7021e52cd26afeeab1cd47506dbefb75
Alex <shaftoe@users.noreply.github.com>
2021-01-11 21:46:19 +0100

    fix: remove cross-spawn dependecy from main script, use native `npm` lib instead (#316) thanks @shaftoe

--- src/helpers/write-help.ts
+++ src/helpers/write-help.ts
@@ -1,19 +1,44 @@
 import chalk from "chalk";
+import path from "path";
+
+export const green = chalk.green;
+export const blue = chalk.blue;
+export const red = chalk.red;
+export const yellow = chalk.yellow;
+export const bold = chalk.bold;
 
 const helpLines = [
   "",
-  `Argument ${chalk.blue("<destination>")} is required.`,
+  `Argument ${blue("<destination>")} is required.`,
   "",
   "Example:",
-  `  ${chalk.green("create-probot-app")} ${chalk.blue("my-first-app")}`,
+  `  ${green("create-probot-app")} ${blue("my-first-app")}`,
   "",
-  `Run ${chalk.green("create-probot-app")} --help to see all options.`,
+  `Run ${green("create-probot-app")} --help to see all options.`,
 ];
 
-function writeHelp(): void {
+export function writeHelp(): void {
   helpLines.forEach((line) => {
     console.log(`  ${line}`);
   });
 }
 
-export default writeHelp;
+export function printSuccess(appName: string, destination: string) {
+  console.log(`
+Successfully created ${blue(appName)}.
+
+Begin using your app with:
+
+  ${green("cd")} ${path.relative(process.cwd(), destination)}
+  npm start
+
+Refer to your app's README for more usage instructions.
+
+Visit the Probot docs
+  https://probot.github.io/docs/
+
+Get help from the community:
+  https://probot.github.io/community/
+
+${green("Enjoy building your Probot app!")}`);
+}
