2df06fda04abd61d68444698b72f837d19b0aa86
Alexander Fortin <shaftoe@users.noreply.github.com>
2021-01-17 20:32:24 +0100

    fix: add some type safety (#335)

--- src/helpers/filesystem.ts
+++ src/helpers/filesystem.ts
@@ -1,30 +1,31 @@
 import path from "path";
 import fs from "fs-extra";
 import { generate } from "egad";
-import { Answers } from "inquirer";
+import { Config } from "./user-interaction";
 import { yellow, green } from "./write-help";
 
 export const templatesSourcePath = path.join(__dirname, "../../templates/");
 
-export async function makeScaffolding(
-  destination: string,
-  answers: Answers,
-  overwrite: boolean
-): Promise<void> {
+/**
+ * Create files and folder structure from Handlebars templates.
+ *
+ * @param {Config} config configuration object
+ */
+export async function makeScaffolding(config: Config): Promise<void> {
   // Prepare template folder Handlebars source content merging `templates/__common__` and `templates/<answers.template>`
   const tempDestPath = fs.mkdtempSync("__create_probot_app__");
   [
     path.join(templatesSourcePath, "__common__"),
-    path.join(templatesSourcePath, answers.template),
+    path.join(templatesSourcePath, config.template),
   ].forEach((source) => fs.copySync(source, tempDestPath));
 
-  const generateResult = await generate(tempDestPath, destination, answers, {
-    overwrite: Boolean(overwrite),
+  const result = await generate(tempDestPath, config.destination, config, {
+    overwrite: config.overwrite,
   });
 
   fs.removeSync(tempDestPath);
 
-  generateResult.forEach((fileInfo) => {
+  result.forEach((fileInfo) => {
     // Edge case: Because create-probot-app is idempotent, if a file is named
     // gitignore in the initializing directory, no .gitignore file will be
     // created.
