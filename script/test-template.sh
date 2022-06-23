faee2147d5df44cb30dd8622dc506d208cf1a5b4
Alexander <alex@Alexanders-MacBook-2021.local>
2022-06-23 10:40:35 +0200

    Fix test scripts to catch every non-zero exit code

--- script/test-template.sh
+++ script/test-template.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-set -Ee # Exit immediately if a command returns a non-zero status
-set -u  # Exit when references variables are undefined
-set -o pipefail  # Exit when any program execution in a pipeline breaks
+. ./script/setup.sh
 
 readonly APP="./bin/create-probot-app.js"
 readonly TEMPLATE=$1
